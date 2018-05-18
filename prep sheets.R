4a72153590e832399afe4f43a37d7e14c7ec4976
judgelord <Devin.JL@gmail.com>
2018-05-18 12:08:21 -0500

    new script formatting sheets for hand coding

---
+++ prep sheets.R
@@ -0,0 +1,21 @@
+# prep sheets for hand coding
+
+sheets <- gs_ls() 
+sheets %<>% filter(author == "correspondenceresearch") %>% select("sheet_title") 
+sheets <- sheets$sheet_title
+
+for(i in sheets) {
+data <- gs_title(i) %>% gs_read()
+
+variables <- c("FROM", "DATE", "SUBJECT", "TYPE", "CERTAINTY", "ALT_TYPE", "POLICY_EVENT", "EVENT_NAME", "EVENT_DATE", "NOTES")
+
+data[ , variables[which(!(variables %in% names(data)))]] <- NA
+
+if(!("ID" %in% names(data))) {
+  data$ID <- seq(1:dim(data)[1])
+}
+
+data %<>% select(ID, DATE, FROM, SUBJECT, TYPE, CERTAINTY, ALT_TYPE, POLICY_EVENT, EVENT_NAME, EVENT_DATE, NOTES, everything())
+
+gs_title(i) %>% gs_edit_cells(input = data, trim = TRUE)
+}
