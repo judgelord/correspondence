b0845a97b182638dbfadf819beca1a1a94a5d5e8
Daniel Kojis <dankojis@gmail.com>
2018-05-25 18:52:53 -0500

    Some minor name issues. Fix later

---
+++ DHHS_CDC.R
@@ -0,0 +1,33 @@
+# This script defines a function clean() for google sheets of correspondence logs that may have been hand coded
+# It may also auto-code variables like TYPE based on agency-specific information
+
+
+# 284 (out of 4363) not matching, go back and fix
+
+#file.name <- "DHHS_CDC" # for testing
+
+
+clean <- function(file.name) {
+  data <- gs_title(file.name) %>% gs_read() # get data
+  
+  ### Remove duplicate IDs
+  data <- data[!duplicated(data$ID), ]
+  
+  #create agency column
+  data$agency <- file.name
+  
+  # Format date, year, Congress, member name etc. 
+  data$DATE %<>% as.Date("%m/%d/%Y")
+  
+  
+  #create year and congress columns
+  data %<>% mutate(year = as.numeric(substring(DATE,1,4) ))
+  data %<>% mutate(congress = as.numeric(round((year - 2001.1)/2)) + 107) # the 107th congress began in 2001
+  
+  # create variable for first and last name
+  data <- getFirstLast.Comma(data, "FROM")
+  
+  
+  # arrange columns for hand coding
+  data %<>% select(ID, DATE, FROM, everything())
+}
