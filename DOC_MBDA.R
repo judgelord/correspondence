670164b465fde49c7be592560ac6acc4a02e7323
DANIEL JOSEPH KOJIS <dkojis@wisc.edu>
2018-05-30 12:37:05 -0500

    update

--- DOC_MBDA.R
+++ DOC_MBDA.R
@@ -1,26 +1,28 @@
 # This script defines a function clean() for google sheets of correspondence logs that may have been hand coded
 # It may also auto-code variables like TYPE based on agency-specific information
 
+# Minor spelling stuff, format rows and subjects yet
 
-#file.name <- "DOC_MBDA" # for testing
+file.name <- "DOC_MBDA" # for testing
+
 
 clean <- function(file.name) {
   data <- gs_title(file.name) %>% gs_read() # get data
   
+  data$ID <- c(1:nrow(data))
   
-  # create agency column
-  data$agency <- file.name
+  #create agency column
+  data$agency <- file.name 
   
+  # # NO DATE PROVIDED
+  data$DATE <- NA
+  # data$DATE %<>% as.Date("%m/%d/%y")
+  # data %<>% mutate(year = as.numeric(substring(DATE,1,4) ))
+  # data %<>% mutate(congress = as.numeric(round((year - 2001.1)/2)) + 107) # the 107th congress began in 2001
+  # 
   
+  # create variable for first and last name
   data <- extractMemberName(data, members, 'FROM')
-  
-  #Create variable for chamber position  (Senator or Representative)
-  data %<>%
-    mutate(chamber = ifelse (grepl("\\(Sen\\)|\\(Sen.\\)", FROM), "Senate", NA)) %>%
-    mutate(chamber = ifelse(grepl("\\(Cong\\)|\\(Cong.\\)", FROM), "House", chamber))
-  
   # arrange columns for hand coding
-  # data %<>% select(ID, DATE, FROM, SUBJECT, chamber, everything())
-  
-  
+  data %<>% select(ID, DATE,  FROM,  everything())
 }
