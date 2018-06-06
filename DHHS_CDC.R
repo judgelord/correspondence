1796f32fd6ffb8c13c48a64326234c1875a25352
judgelord <devin.jl@gmail.com>
2018-06-06 11:23:45 -0500

    fixed different date formats

--- DHHS_CDC.R
+++ DHHS_CDC.R
@@ -5,7 +5,7 @@
 # 1204 out of 1316 matches. I think all non-matches are non-members after checking, should be good. 
 # Complete
 
- file.name <- "DHHS_CDC" # for testing
+# file.name <- "DHHS_CDC" # for testing
 
 
 clean <- function(file.name) {
@@ -18,7 +18,9 @@ clean <- function(file.name) {
   data$agency <- file.name
   
   # Format date, year, Congress, member name etc. 
-  data$DATE %<>% as.Date("%m/%d/%Y")
+  data$DATE <- gsub("/201", "/1", data$DATE)
+  data$DATE <- gsub("/200", "/0", data$DATE)
+  data$DATE %<>% as.Date("%m/%d/%y")
   
   
   #create year and congress columns
