eb305bfae1e7062e426985c6f646fb5b6c3f91ce
Daniel Kojis <dankojis@gmail.com>
2018-06-17 14:10:06 -0500

    In progress

---
+++ NCUA.R
@@ -0,0 +1,40 @@
+# This script defines a function clean() for google sheets of correspondence logs that may have been hand coded
+# It may also auto-code variables like TYPE based on agency-specific information
+
+
+#file.name <- "NCUA" # for testing
+
+clean <- function(file.name) {
+  data <- gs_title(file.name) %>% gs_read() # get data
+  
+  
+  # create agency column
+  data$agency <- file.name
+  
+  # Format date, year, Congress, member name etc. 
+  data$DATE %<>% as.Date("%m/%d/%Y")
+  
+  
+  #create year and congress columns
+  data %<>% mutate(year = as.numeric(substring(DATE,1,4) ))
+  data %<>% mutate(congress = as.numeric(round((year - 2001.1)/2)) + 107) # the 107th congress began in 2001
+  
+  
+  # create first and last name variables
+  data <- extractMemberName(data, members, 'FROM')
+  
+  
+  
+  # arrange columns for hand coding
+  data %<>% select(ID, DATE, FROM, SUBJECT, everything())
+  
+  
+  
+  
+}
+
+
+
+
+
+
