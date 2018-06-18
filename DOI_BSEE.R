86dac28aed3b1bc7b1918594496e8deb66597160
Aaron Gold <AaronGold@Aarons-MacBook-Air.local>
2018-06-18 12:31:59 -0500

    STARTED CODING DOI_BSEE

--- DOI_BSEE.R
+++ DOI_BSEE.R
@@ -58,4 +58,16 @@ clean <- function(file.name) {
   
   # arrange columns for hand coding
   data %<>% select(ID, DATE,  FROM, chamber, everything())
+  
+  data%<>%
+  mutate(TYPE = ifelse (!grepl("[0-9]", TYPE) & grepl("CONSTITUENT", SUBJECT, ignore.case = TRUE), "1", TYPE)) %>%
+  mutate(CERTAINTY = ifelse (!grepl("[0-9]", CERTAINTY) & grepl("CONSTITUENT", SUBJECT, ignore.case = TRUE), "1", CERTAINTY))
+  
+  
+  
+  
+  
+  
+  
+  
 }
