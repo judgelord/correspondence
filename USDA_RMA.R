8507367f94d0d16c6b7bd08c8cb53aa35eae758a
Aaron Gold <AaronGold@Aarons-MacBook-Air.local>
2018-06-19 15:19:59 -0500

    Coded USDA_RMA, some hand coding may be necessary

--- USDA_RMA.R
+++ USDA_RMA.R
@@ -62,8 +62,28 @@ clean <- function(file.name) {
   data %<>% select(ID, DATE,  FROM, everything())
   
   data%<>%
-  mutate(TYPE = ifelse (!grepl("[0-9]", TYPE) & grepl("CLAIM", SUBJECT, ignore.case = TRUE), "1", TYPE)) %>%
-  mutate(CERTAINTY = ifelse (!grepl("[0-9]", CERTAINTY) & grepl("IN FICON", SUBJECT, ignore.case = TRUE), "2", CERTAINTY)) %>%
+  mutate(TYPE = ifelse (!grepl("[0-9]", TYPE) & grepl("CLAIM|LATE PAYMENT|PREMIUM|PREVENTED PLANTING|FRAUD|ITS|ROTAT", SUBJECT, ignore.case = TRUE), "1", TYPE)) %>%
+  mutate(CERTAINTY = ifelse (!grepl("[0-9]", CERTAINTY) & grepl("CLAIM|LATE PAYMENT|PREMIUM|PREVENTED PLANTING|FRAUD|ITS||ROTAT", SUBJECT, ignore.case = TRUE), "3", CERTAINTY)) %>%
+  mutate(ALT_TYPE = ifelse (!grepl("[0-9]", ALT_TYPE) & grepl("CLAIM|LATE PAYMENT|PREMIUM|PREVENTED PLANTING|FRAUD|ITS|ROTAT", SUBJECT, ignore.case = TRUE), "2", ALT_TYPE)) %>%
+  mutate(NOTES = ifelse (!grepl("[0-9]", NOTES) & grepl("CLAIM", SUBJECT, ignore.case = TRUE), "THE RMA PROVIDES US FARMERS CROP INSURANCE, NOT SURE IF PROVIDING THIS TO FARMERS WOULD BE CONSIDERED INDIVIDUAL SERVICE OR COMMERCIAL SERVICE", NOTES)) %>%
+  mutate(TYPE = ifelse (!grepl("[0-9]", TYPE) & grepl("TOBACCO|WHEAT|COTTON|SPECIALTY CROP", SUBJECT, ignore.case = TRUE), "2", TYPE)) %>%
+  mutate(CERTAINTY = ifelse (!grepl("[0-9]", CERTAINTY) & grepl("TOBACCO|WHEAT|COTTON|SPECIALTY CROP", SUBJECT, ignore.case = TRUE), "2", CERTAINTY)) %>%
+  mutate(ALT_TYPE = ifelse (!grepl("[0-9]", ALT_TYPE) & grepl("TOBACCO|WHEAT|COTTON|SPECIALTY CROP", SUBJECT, ignore.case = TRUE), "4", ALT_TYPE)) %>%
+  mutate(TYPE = ifelse (!grepl("[0-9]", TYPE) & grepl("PLANTING DATE", SUBJECT, ignore.case = TRUE), "4", TYPE)) %>%
+  mutate(CERTAINTY = ifelse (!grepl("[0-9]", CERTAINTY) & grepl("PLANTING DATE", SUBJECT, ignore.case = TRUE), "1", CERTAINTY)) %>%
+  mutate(TYPE = ifelse (!grepl("[0-9]", TYPE) & grepl("BOARD NOMINATION", SUBJECT, ignore.case = TRUE), "5", TYPE)) %>%
+  mutate(CERTAINTY = ifelse (!grepl("[0-9]", CERTAINTY) & grepl("BOARD NOMINATION", SUBJECT, ignore.case = TRUE), "1", CERTAINTY)) %>%
+  mutate(EVENT_NAME = ifelse (!grepl("[0-9]", EVENT_NAME) & grepl("BOARD NOMINATION", SUBJECT, ignore.case = TRUE), "NOMINATION", EVENT_NAME)) %>%
+  mutate(TYPE = ifelse (!grepl("[0-9]", TYPE) & grepl("GRANT PROGRAM|GRANT", SUBJECT, ignore.case = TRUE), "3", TYPE)) %>%
+  mutate(CERTAINTY = ifelse (!grepl("[0-9]", CERTAINTY) & grepl("GRANT PROGRAM|GRANT", SUBJECT, ignore.case = TRUE), "2", CERTAINTY)) %>%
+  mutate(ALT_TYPE = ifelse (!grepl("[0-9]", ALT_TYPE) & grepl("GRANT PROGRAM|GRANT", SUBJECT, ignore.case = TRUE), "2", ALT_TYPE)) %>%
+  mutate(TYPE = ifelse (!grepl("[0-9]", TYPE) & grepl("STUDY|REPORT|ARBITRATION|INSURANCE POLICY", SUBJECT, ignore.case = TRUE), "5", TYPE)) %>%
+  mutate(CERTAINTY = ifelse (!grepl("[0-9]", CERTAINTY) & grepl("STUDY|REPORT|ARBITRATION|INSURANCE POLICY", SUBJECT, ignore.case = TRUE), "1", CERTAINTY)) %>%
+  mutate(POLICY_EVENT = ifelse (!grepl("[0-9]", POLICY_EVENT) & grepl("STUDY|REPORT", SUBJECT, ignore.case = TRUE), "INFORMATION", POLICY_EVENT)) %>%
+  mutate(TYPE = ifelse (!grepl("[0-9]", TYPE) & grepl("PRF|IOWA", SUBJECT, ignore.case = TRUE), "3", TYPE)) %>%
+  mutate(CERTAINTY = ifelse (!grepl("[0-9]", CERTAINTY) & grepl("PRF|IOWA", SUBJECT, ignore.case = TRUE), "1", CERTAINTY)) %>%
+  mutate(TYPE = ifelse (!grepl("[0-9]", TYPE) & grepl("FOIA", SUBJECT, ignore.case = TRUE), "1", TYPE)) %>%
+  mutate(CERTAINTY = ifelse (!grepl("[0-9]", CERTAINTY) & grepl("FOIA", SUBJECT, ignore.case = TRUE), "1", CERTAINTY))
     
   
   
