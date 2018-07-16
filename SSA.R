b05a38be3536ffb32ff344a092fad4fff38bce52
Aaron Gold <AaronGold@Aarons-Air.lan>
2018-07-16 15:52:35 -0500

    almost done coding SSA

--- SSA.R
+++ SSA.R
@@ -28,11 +28,25 @@ clean <- function(file.name) {
   
   data %<>%
   mutate(SUBJECT = paste(SUBJECT,ACTION)) %>% 
-  mutate(TYPE = ifelse (!grepl("[0-9]", TYPE) & grepl("UNDERPAYMENT|INITIAL CLAIM|CITIZENSHIP|OVERPAYMENT", SUBJECT, ignore.case = TRUE), "1", TYPE)) %>%
-  mutate(CERTAINTY = ifelse (!grepl("[0-9]", CERTAINTY) & grepl("UNDERPAYMENT|INITIAL CLAIM|CITIZENSHIP|OVERPAYMENT", SUBJECT, ignore.case = TRUE), "1", CERTAINTY)) %>%
-  mutate(TYPE = ifelse (!grepl("[0-9]", TYPE) & grepl("FIRST DRAFT", SUBJECT, ignore.case = TRUE), "5", TYPE)) %>%
-  mutate(CERTAINTY = ifelse (!grepl("[0-9]", CERTAINTY) & grepl("FIRST DRAFT", SUBJECT, ignore.case = TRUE), "1", CERTAINTY)) %>%
-  mutate(POLICY_EVENT = ifelse (!grepl("[0-9]", POLICY_EVENT) & grepl("FIRST DRAFT", SUBJECT, ignore.case = TRUE), "LEGISLATION", POLICY_EVENT))
+  mutate(SUBJECT = paste(SUBJECT, CCRS.Specialist)) %>%
+  mutate(TYPE = ifelse (!grepl("[0-9]", TYPE) & grepl("UNDERPAYMENT|INITIAL CLAIM|CITIZENSHIP|OVERPAYMENT|CONSTITUENT|DISABILITY|DENIED|STATUS ON CLAIM|RETIREMENT|WIDOW|BILLING|HIRING PRACTICE|WITHDRAWAL|2ND REQUEST|SUICIDE|INITIAL AWARD|ATTORNEY FEE|ADDITIONAL PAYMENT", SUBJECT, ignore.case = TRUE), "1", TYPE)) %>%
+  mutate(CERTAINTY = ifelse (!grepl("[0-9]", CERTAINTY) & grepl("UNDERPAYMENT|INITIAL CLAIM|CITIZENSHIP|OVERPAYMENT|CONSTITUENT|DISABILITY|DENIED|STATUS ON CLAIM|RETIREMENT|WIDOW|BILLING|HIRING PRACTICE|WITHDRAWAL|2ND REQUEST|SUICIDE|INITIAL AWARD|ATTORNEY FEE|ADDITIONAL PAYMENT", SUBJECT, ignore.case = TRUE), "1", CERTAINTY)) %>%
+  mutate(TYPE = ifelse (!grepl("[0-9]", TYPE) & grepl("FIRST DRAFT|HEARING|RULE|ADCLCA TO CONTROL|REQUEST FOR INFORMATION|ALJ DECISION|RECONSIDERATION DETERMINATION", SUBJECT, ignore.case = TRUE), "5", TYPE)) %>%
+  mutate(CERTAINTY = ifelse (!grepl("[0-9]", CERTAINTY) & grepl("FIRST DRAFT|HEARING|RULE|ADCLCA TO CONTROL|REQUEST FOR INFORMATION|ALJ DECISION|RECONSIDERATION DETERMINATION", SUBJECT, ignore.case = TRUE), "1", CERTAINTY)) %>%
+  mutate(POLICY_EVENT = ifelse (!grepl("[0-9]", POLICY_EVENT) & grepl("FIRST DRAFT", SUBJECT, ignore.case = TRUE), "LEGISLATION", POLICY_EVENT)) %>%
+  mutate(TYPE = ifelse (!grepl("[0-9]", TYPE) & grepl("OFFICIAL BUSINESS", SUBJECT, ignore.case = TRUE), "2", TYPE)) %>%
+  mutate(CERTAINTY = ifelse (!grepl("[0-9]", CERTAINTY) & grepl("OFFICIAL BUSINESS", SUBJECT, ignore.case = TRUE), "1", CERTAINTY)) %>%
+  mutate(POLICY_EVENT = ifelse (!grepl("[0-9]", POLICY_EVENT) & grepl("HEARING", SUBJECT, ignore.case = TRUE), "HEARING", POLICY_EVENT)) %>%
+  mutate(TYPE = ifelse (!grepl("[0-9]", TYPE) & grepl("CLAIM|BENEFITS|COMPLAINT|APPEAL PENDING|STAFFER", SUBJECT, ignore.case = TRUE), "1", TYPE)) %>%
+  mutate(CERTAINTY = ifelse (!grepl("[0-9]", CERTAINTY) & grepl("CLAIM|BENEFITS|COMPLAINT|APPEAL PENDING|STAFFER", SUBJECT, ignore.case = TRUE), "1", CERTAINTY)) %>%
+  mutate(POLICY_EVENT = ifelse (!grepl("[0-9]", POLICY_EVENT) & grepl("RULE", SUBJECT, ignore.case = TRUE), "RULE", POLICY_EVENT)) %>%
+  mutate(POLICY_EVENT = ifelse (!grepl("[0-9]", POLICY_EVENT) & grepl("ADCLCA|ALJ DECISION|RECONSIDERATION DETERMINATION", SUBJECT, ignore.case = TRUE), "DECISION", POLICY_EVENT)) %>%
+  mutate(TYPE = ifelse (!grepl("[0-9]", TYPE) & grepl("DEBT RECOVERY", SUBJECT, ignore.case = TRUE), "1", TYPE)) %>%
+  mutate(CERTAINTY = ifelse (!grepl("[0-9]", CERTAINTY) & grepl("DEBT RECOVERY", SUBJECT, ignore.case = TRUE), "3", CERTAINTY)) %>%
+  mutate(ALT_TYPE = ifelse (!grepl("[0-9]", ALT_TYPE) & grepl("DEBT RECOVERY", SUBJECT, ignore.case = TRUE), "5", ALT_TYPE)) %>%
+  mutate(POLICY_EVENT = ifelse (!grepl("[0-9]", POLICY_EVENT) & grepl("REQUEST FOR INFORMATION", SUBJECT, ignore.case = TRUE), "INFORMATION", POLICY_EVENT)) %>%
+  mutate(TYPE = ifelse (!grepl("[0-9]", TYPE) & grepl("LUMBERTON", SUBJECT, ignore.case = TRUE), "3", TYPE)) %>%
+  mutate(CERTAINTY = ifelse (!grepl("[0-9]", CERTAINTY) & grepl("LUMBERTON", SUBJECT, ignore.case = TRUE), "1", CERTAINTY))
   
   
   
