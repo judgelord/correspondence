4cfe42bab4311bbc0cf3f75add534d53eec3f81c
DANIEL JOSEPH KOJIS <dkojis@wisc.edu>
2018-08-20 09:30:43 -0500

    update

--- scratchpad/FAApdf.R
+++ scratchpad/FAApdf.R
@@ -15,7 +15,17 @@ data %<>%
   separate(SignedDate, into= c("SignedDate", "Analyst"), sep= "Analyst: ") %>% 
   separate(Analyst, into= c("Analyst", "Writer/Editor"), sep= "Writer/Editor: ") %>% 
   separate(`Writer/Editor`, into= c("Writer/Editor", "Significant"), sep= "Significant:") %>% 
-  separate(`Significant`, into= c("Significant", "Folder Template"), sep= "Folder Template: ") 
+  separate(`Significant`, into= c("Significant", "Folder Template"), sep= "Folder Template: ") %>% 
+  separate(`Folder Template`, into= c("Folder Template", "Addressed To"), sep= "Addressed To:") %>% 
+  separate(`Addressed To`, into= c("Addressed To", "On Behalf Of"), sep= "On Behalf Of:") %>% 
+  separate(`On Behalf Of`, into= c("On Behalf Of", "Cross Reference"), sep= "Cross Reference:") %>% 
+  separate(`Cross Reference`, into= c("Cross Reference", "White House Reference"), sep= "White House Reference:") %>% 
+  separate(`White House Reference`, into= c("White House Reference", "Relate Control"), sep= "Related Control:") 
+
+
+
+
+
 
 
 
