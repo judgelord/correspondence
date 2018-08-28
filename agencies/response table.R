bd59276974729e377422e6546ae4728693c14536
judgelord <devin.jl@gmail.com>
2018-08-28 15:44:52 -0500

    update pres

--- agencies/response table.R
+++ agencies/response table.R
@@ -1,3 +1,7 @@
+
+
+
+# get FOIAed agency totals from FOIA List on drive 
 data <- gs_title("FOIA List") %>% gs_read() %>% select(Department, Bureau, sample, data, on_drive) %>% filter(!is.na(sample)) %>% distinct() #%>% group_by(agency) %>% tally()
 
 data$data <-gsub("yes.*", 1, data$data)
@@ -18,6 +22,21 @@ data %<>% mutate(Department = ifelse(!grepl("Department of", Department), "Indep
 data %<>% group_by(Department) %>% mutate(Components = n(), Records = sum(data1), Coded = sum(on_drive)) %>% distinct()
 
 data %<>% group_by(Department, Components, Records, Coded) %>% tally() %>% select(-n)
+
+# 
+# # get letter totals from df 
+# dfdata <- df %<>% mutate(Department = ifelse(!grepl("Department of", Department), "Independent Agencies", Department))
+# 
+# dfdata %<>% group_by(Department) %>% summarise(n = n() ) %>% distinct()
+# 
+# # join foia sheet data with r data 
+# data %<>% full_join(dfdata) 
+# 
+# data %<>% mutate(n = ifelse(is.na(n), 0, n))
+
+# totals 
+data %<>% ungroup() %>%
+  bind_rows(summarise_all(., funs(if(is.numeric(.)) sum(.) else "Total")))
 
 library(stargazer)
 
