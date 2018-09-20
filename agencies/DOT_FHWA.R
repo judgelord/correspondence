cad4866d9ac4fbb74825f32920902e68ceab74e2
DANIEL JOSEPH KOJIS <dkojis@wisc.edu>
2018-09-19 19:36:33 -0500

    update

--- agencies/DOT_FHWA.R
+++ agencies/DOT_FHWA.R
@@ -1,9 +1,7 @@
 # This script defines a function clean() for google sheets of correspondence logs that may have been hand coded
 # It may also auto-code variables like TYPE based on agency-specific information
 
-
-
-# file.name <- "DOT_FHWA" # for testing
+#file.name <- "DOT_FHWA" # for testing
 
 
 # Duplicates need to be addressed
