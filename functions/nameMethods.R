eddc5bb9e376dd8eba0636b397f882f3e86b0200
DANIEL JOSEPH KOJIS <dkojis@wisc.edu>
2018-10-27 14:14:04 -0500

    update

--- functions/nameMethods.R
+++ functions/nameMethods.R
@@ -819,7 +819,7 @@ ocr.errors <- function(FROM){
   FROM <- gsub("(^| )Martrin( |$)", "\\1Martin\\2", FROM, ignore.case = TRUE)
   FROM <- gsub("(^| )Machin( |$)", "\\1Manchin\\2", FROM, ignore.case = TRUE)
   
-  Machin
+  
   
   return(FROM)
 }
