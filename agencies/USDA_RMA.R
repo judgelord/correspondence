151de87d61ef6620fda5410914a51837878fc1c0
DANIEL JOSEPH KOJIS <dkojis@wisc.edu>
2019-02-02 16:05:53 -0600

    update

--- agencies/USDA_RMA.R
+++ agencies/USDA_RMA.R
@@ -2,7 +2,7 @@
 # It may also auto-code variables like TYPE based on agency-specific information
 
 
-# file.name <- "USDA_RMA" # for testing
+#file.name <- "USDA_RMA" # for testing
 
 
 clean <- function(file.name) {
