27395d361b8b80d6e930fcd7aaf63247371c8095
FATIMA SABIRIN ALI <fsali@wisc.edu>
2019-06-12 17:00:25 -0500

    Checked DOD_OSDJS for errors

--- merge.R
+++ merge.R
@@ -156,7 +156,7 @@ data_list
 # initialize for full merge (default)
 i <- 1
 # or choose one agency
-i <- which(data_list$agency == "Treasury_Fiscal") 
+i <- which(data_list$agency == "DOD_OSDJS") 
 d1 <- clean.agency(agency = data_list[i, 1],
                      status = data_list[i, 2],
                      coders = data_list[i, 3])
