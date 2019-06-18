a2ef2ceb18785c5392cf89367223778b64f56d35
Maggie-Nead <mnead@wisc.edu>
2019-06-18 12:17:47 -0500

    Fixed date format added to string split coded for extract member names made notes and errors for non members and multiple members

--- agencies/CNCS.R
+++ agencies/CNCS.R
@@ -11,29 +11,25 @@ clean <- function(file.name) {
   #Create ID
   data %<>%
     mutate(ID = row_number())
-  
 
   
   data %<>%
     mutate(DATE = ifelse(is.na(DATE), Out, DATE))
-  
-  # Format date, year, Congress, member name etc. 
-  data$DATE <- gsub("/201", "/1", data$DATE) 
-  data$DATE <- gsub("/200", "/0", data$DATE)
-  data$DATE %<>% as.Date("%m/%d/%y")
-  
-  
+
 
   #Checking for missing dates
   NAdate<-data %>%
     filter(is.na(DATE))
   
   #create year and congress columns
+  data$DATE %<>% as.Date("%m/%d/%y")
   data %<>% mutate(year = as.numeric(substring(DATE,1,4) ))
   data %<>% mutate(congress = as.numeric(round((year - 2001.1)/2)) + 107) # the 107th congress began in 2001
   
+  #data %<>%
+    #mutate(FROM = ifelse(str_replace()))
   data %<>%
-    mutate(FROM = str_split(FROM, "\\/|&|;| and")) %>%
+    mutate(FROM = str_split(FROM, "\\/|&|;| and|Rep. |Sen. |(S), |(CW), |(CM), ")) %>%
     unnest(FROM)
   
   data %<>%
@@ -42,15 +38,39 @@ clean <- function(file.name) {
   
   data %<>%
     mutate(FROM = str_remove(FROM, "Sen. ")) %>%
-    mutate(FROM = str_remove(FROM, "Rep. "))
+    mutate(FROM = str_remove(FROM, "Rep. |\\)|\\(|Reps |Sen "))
+             
   
   data %<>% select(ID, DATE, FROM, everything())  
 
   
   data <- getFirstLast.Comma(data, 'FROM')
   
-  Unfoundnames <- data %>%
+  data %<>% filter(!FROM == "")
+  
+  Unfound <- data %>%
     filter(is.na(last_name))
+  
+  #Extracts member names from NAs in getfirstlast
+  Unfoundnames <- data %>%
+    filter(is.na(last_name)) %>%
+    extractMemberName(members = members, col_name = "FROM")
+  
+  #Drops duplicate observations  
+  data %<>%
+    drop_na(last_name)
+  
+  #Rejoins data
+  data %<>%
+    full_join(Unfoundnames)
+  
+  #Checks for observations still NA
+  notfound2 <- data %>%
+    filter(is.na(last_name))
+  
+  data %<>%
+    mutate(NOTES = ifelse(str_detect(Title, "Multi"), "Multiple unnamed members", NOTES)) %>%
+    mutate(ERROR = ifelse(str_detect(Title, "Gov"), "State Governor", ERROR))
   
   return(data)
   
