7a0e89438048cf8d78aee7f8674efcdfdfb35ea5
Maggie-Nead <mnead@wisc.edu>
2019-06-21 13:48:24 -0500

    Started script created ID, chamber and agency column formatted date string split to separate members extracted member names

---
+++ agencies/CSOSA.R
@@ -0,0 +1,67 @@
+#This script defines a function clean() for google sheets of correspondence logs that may have been hand coded
+# It may also auto-code variables like TYPE based on agency-specific information
+
+#file.name <- "CSOSA" # for testing
+
+clean <- function(file.name) {
+  data <- gs_title(file.name) %>% gs_read() %>% distinct()# get data
+  
+  #Create ID
+  data %<>%
+    mutate(ID = row_number())
+  
+  #create agency column
+  data$agency <- file.name 
+  
+  
+  
+  #Make ??? NA
+ #Format Date
+  data %<>%
+    mutate(DATE = if_else(is.na(DATE), `Date of Reply/Contact`, DATE))
+  data$DATE %<>% as.Date("%d/%m/%y")
+  
+  #create year and congress columns
+  data %<>% mutate(year = as.numeric(substring(DATE,1,4) ))
+  data %<>% mutate(congress = as.numeric(round((year - 2001.1)/2)) + 107) # the 107th congress began in 2001
+  
+  
+  #chamber
+  data %<>%
+    mutate(chamber = ifelse(str_detect(FROM, "House"), "House", NA)) %>%
+    mutate(chamber = ifelse(str_detect(FROM, "Senate"), "Senate", chamber))
+  
+  #String split to separate members
+  data %<>%
+    mutate(FROM = str_split(FROM, "\\/|&")) %>%
+    unnest(FROM)
+  
+  data %<>%
+    mutate(FROM = str_remove(FROM, "Senate|House|. Chair|V. Chair|Chair|, OMB| OMB|Sender's Information"))
+  
+
+  #extracts member names
+  data %<>%
+    extractMemberName(members = members, col_name = "FROM")
+  
+  data %<>%
+    mutate(FROM = str_trim(FROM)) %>%
+    mutate(last_name = ifelse(! str_detect(FROM, " ") & is.na(last_name), formatLastName(data, 'FROM'), last_name))
+ 
+  data %<>% filter( ! FROM == "")
+  
+  data %<>%
+    mutate(Blank = is.na(FROM) & is.na(SUBJECT)) %>%
+    filter(! Blank)
+ 
+
+  data %<>%
+    mutate(NOTES = ifelse(str_detect(FROM, "Committee"), "Committee", NOTES))
+  
+   data %<>% select(ID, DATE,  FROM, last_name, chamber, SUBJECT, everything())
+  
+  
+  
+  
+  return(data)
+}
