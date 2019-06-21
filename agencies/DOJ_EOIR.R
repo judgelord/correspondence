b7a3b58d77f1701a49c41e3c6db40a27b99464b2
Maggie-Nead <mnead@wisc.edu>
2019-06-21 15:53:43 -0500

    Fixed name error, Commented errors for state legislators filtered for unmatched names

--- agencies/DOJ_EOIR.R
+++ agencies/DOJ_EOIR.R
@@ -27,16 +27,35 @@ clean <- function(file.name) {
   data %<>%
     mutate(FROM = str_split(FROM, " and |&")) %>%
     unnest(FROM)
+
+  data %<>%
+    mutate(FROM = str_replace(FROM, "Con Jim Morgan", "Con Jim Moran"))
+ 
+  #Remove middle initials for now
+  
   
   data %<>%
-    extractMemberName(members = members, col_name = "FROM") %>% 
+    extractMemberName(members = members, col_name = "FROM")
+  
+  
+  #Unmatched
+  unmatched <- data %>%
+    filter(is.na(last_name)) %>%
     select(DATE, FROM, first_name, last_name, SUBJECT, everything())
   
+  #Subject catching non authors
+  #Filters for names still unmatched
+  #Unfoundnames <- data %>%
+   # filter(is.na(last_name)) %>%
+    #extractMemberName(members = members, col_name = "SUBJECT") %>% 
   
   
   
-  
-  
+  data %<>%
+    mutate(ERROR = ifelse(str_detect(FROM, "Don Tripp, State Representative of New Mexico|Daniel Dromm, New York City Council Member|Eva Galambos, Mayor of Sandy Springs, Georgia|John M. Kefalas, State Representative of Colorado|Elaine Nekritz, Illinois State Representative - 57th District|
+                                     John J. Gleason, State Senator of Michigan, 27th District|Rashida H. Tlaib, State Representative of Michigan|
+                                     Sen Leticia Van de Putte, R.PH., State of Texas, District 26|Amanda Aguirre, Senator, District 24, Arizona State Senate|Willie Simmons, State Senator of Mississpi|
+                                     Daphne Campell, RN, State Representative of Fl, District 108|Sen Noreen Evans, California State Senate, Second Senate District"), "State Legislator", ERROR))
   
   
   
