284252753f1bbcbdc624636ee67056708ed32fde
judgelord <devin.jl@gmail.com>
2019-07-01 09:59:13 -0500

    add members2 to nameCongress data

--- scratchpad/nameMethods compared.R
+++ scratchpad/nameMethods compared.R
@@ -1,16 +1,12 @@
 look1 <- data %>% extractMemberName(members, "FROM") %>% mutate(method = 1)
 look2 <- data %>% extractMemberName2(members, "FROM") %>% mutate(method = 2)
 
-problems <- full_join(look1,# %>% select(-FROM2), 
-                      look2) %>% # %>% select(-pattern)) %>% 
-  select(FROM, typos, correct, first_name, last_name, pattern, congress) %>% 
+problems <- full_join(look1,
+                      look2) %>% 
+  select(FROM, typos, correct, first_name, last_name, string, pattern, congress) %>% 
   distinct() %>% 
   filter(!is.na(FROM), FROM != "", FROM != "NA") %>% 
   arrange(FROM)
-
-
-members2 <- full_join(members, members_106to109th) 
-
 
 p <- problems %>% # and merge with voteview data
   left_join(members2) %>% # merge on common variables (may differ)
