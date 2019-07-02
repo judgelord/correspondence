172bb252f238f3b4aad2a48e026b6e78e2324049
judgelord <devin.jl@gmail.com>
2019-07-02 18:49:41 -0500

    change FTC to extractMemberNames only

--- scratchpad/nameMethods compared.R
+++ scratchpad/nameMethods compared.R
@@ -1,22 +1,31 @@
-look1 <- data %>% top_n(100, DATE) %>% extractMemberName(members, "FROM") %>% mutate(method = 1, ID = as.numeric(ID))
-look2 <- data %>% top_n(100, DATE) %>% extractMemberName2(members, "FROM") %>% mutate(method = 2, ID = as.numeric(ID))
 
-look <- full_join(look1, look2)
+# a 5% sample 
+sample <- sample_frac(data, .05)
 
-problems <- look %>% 
-  select(ID, FROM, typos, correct, first_name, last_name, string, pattern, congress) %>% 
+# run both methods
+method_1 <- sample %>% extractMemberName(members, "FROM") %>% mutate(method = 1, ID = as.numeric(ID))
+method_2 <- sample %>% extractMemberName2(members, "FROM") %>% mutate(method = 2, ID = as.numeric(ID))
+
+# combine results
+methods_compared <- full_join(method_1, method_2) %>% arrange(FROM)
+
+method_problems <- methods_compared %>% 
+  select(ID, FROM, first_name, last_name, congress) %>% 
   # drop observations that matched or did not match in both methods
   distinct() %>% 
   filter(!is.na(FROM), FROM != "", FROM != "NA") %>% 
   count(ID) %>% 
-  # subset to observations that were distinct
+  # subset to observations that were distinct in the two methods
   filter(n >1) %>% 
-  # join back in data 
-  left_join(look)%>% 
+  # join back in data for these observations
+  left_join(methods_compared)%>% 
   arrange(FROM) %>% 
   select(FROM, string, pattern, first_name, last_name, method,  congress) 
 
-should_be_matching <- problems %>% 
+# is extractMemberName2 better? 
+nrow(method_problems %>% filter(method == 1, is.na(last_name))) > nrow(method_problems %>% filter(method == 1), is.na(last_name)) 
+
+method_2_should_be_matching <- method_problems %>% 
   filter(is.na(last_name), 
          pattern != "404error") %>% 
   drop_na(pattern) %>% 
