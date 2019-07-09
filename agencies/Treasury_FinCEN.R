c81d011a7b41a03fa54cbdd8d0612e333dcc1bb8
fsabirinali <f.sabirin.ali@gmail.com>
2019-07-09 15:07:58 -0500

    Editing Fin_CEN errors

--- agencies/Treasury_FinCEN.R
+++ agencies/Treasury_FinCEN.R
@@ -98,7 +98,7 @@ clean <- function(file.name) {
     mutate(Summary = str_replace(Summary, " Mack", "MACK, Connie")) %>%
     mutate(Summary = str_replace(Summary, "Sherman", "Representative Sherman")) %>%
     mutate(Summary = str_replace(Summary, "Feinstein", "Senator Feinstein")) %>%
-    mutate(Summary = str_replace(Summary, "Ed Perimutter", "Ed PERLMUTTER "))
+    mutate(Summary = str_replace(Summary, "Ed Perimutter", "Ed PERLMUTTER ")) #added to nameMethods
   
   #Trim White Space
   data %<>%
@@ -146,7 +146,7 @@ clean <- function(file.name) {
   # sample <- data %>%
   # filter(is.na(last_name))  
   # View(sample) 
-  # 
+  
   
   
   return(data)
