a55fc44e3079d67bdf61430104256ff60ef12f4a
Maggie-Nead <mnead@wisc.edu>
2019-07-10 15:45:22 -0500

    Deleted code that caused error in merge

--- agencies/DOT_PHMSA.R
+++ agencies/DOT_PHMSA.R
@@ -26,7 +26,7 @@ clean <- function(file.name) {
   data %<>% mutate(year = as.numeric(substring(DATE,1,4) ))
   data %<>% mutate(congress = as.numeric(round((year - 2001.1)/2)) + 107) # the 107th congress began in 2001
   
-  data %<>% select(ID, DATE, FROM, everything())  
+ 
   
   #chamber
   data %<>%
