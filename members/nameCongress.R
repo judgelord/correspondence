affee6dc56a227b86ca05a34f85eeb346ccafbdb
fsabirinali <f.sabirin.ali@gmail.com>
2019-07-11 14:36:45 -0500

    Adding middle name for Tina smith

--- members/nameCongress.R
+++ members/nameCongress.R
@@ -292,6 +292,7 @@ members <- full_join(member_search(congress = c(105:108)) %>% select(-congresses
     mutate(middle_name = ifelse(bioname == "THOMPSON, Glenn", "William", middle_name)) %>%  
     mutate(middle_name = ifelse(bioname == "PRICE, Tom", "Edmunds", middle_name)) %>%  
     mutate(middle_name = ifelse(bioname == "RICE, Tom", "Thompson", middle_name)) %>%  
+    mutate(middle_name = ifelse(bioname == "SMITH, Tina", "Elizabeth", middle_name)) %>%  
      
     # middle initials
     mutate(middle_initial = ifelse(bioname == "CASEY, Robert (Bob), Jr.", "P", middle_initial)) %>% 
