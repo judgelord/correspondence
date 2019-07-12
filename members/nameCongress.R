a274b4a00736826c5d68665a6891f33fa942c9e6
fsabirinali <f.sabirin.ali@gmail.com>
2019-07-12 15:41:09 -0500

    Adding common name for Lacy Clay

--- members/nameCongress.R
+++ members/nameCongress.R
@@ -253,6 +253,7 @@ members <- full_join(member_search(congress = c(105:108)) %>% select(-congresses
     mutate(common_name = ifelse(bioname == "PORTMAN, Robert Jones (Rob)", "(Bob|Rob)", common_name)) %>%
     mutate(common_name = ifelse(bioname == "PANETTA, James Varni", "(Jimmy|Jim)", common_name)) %>%
     mutate(common_name = ifelse(bioname == "CISNEROS, Gil", "Gilbert", common_name)) %>%
+    mutate(common_name = ifelse(bioname == "CLAY, William Lacy, Jr.", "(Bill|Lacy)", common_name)) %>%
      
   # remove accent marks (using RegEx dot for specials, not exact matching)
     mutate(common_name = ifelse(grepl("GRIJALVA, Ra.l M.", bioname), "Raul", common_name)) %>% 
