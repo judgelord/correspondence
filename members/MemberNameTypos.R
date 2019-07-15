d5a6e7020fc5385f6b5f91b261b81189799a2722
fsabirinali <f.sabirin.ali@gmail.com>
2019-07-15 13:08:43 -0500

    Fixing name error for O'Halleran

--- members/MemberNameTypos.R
+++ members/MemberNameTypos.R
@@ -225,7 +225,7 @@ typos_last <- tribble(
   "Nanette", "Diaz Barragan", "Diaz-Barragan",
   "Bonnie", "Watson Coleman", "Watson-Coleman",
   "Adrian", "Espaillat", "Espaillet",
-  "Tony", "O'Holleran", "O'Halleran",
+  "Tony", "O'Halleran", "O'Holleran",
   "Anthony", "BRINDISI", "Brindis",
   "Lisa", "BLUNT ROCHESTER", "BLUNT-ROCHESTER",
   "Bruce", "Braley", "Braly"
