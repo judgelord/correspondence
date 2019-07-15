1ebe4153f6cc8e3ccf51d1c0aba15bd092741d8c
fsabirinali <f.sabirin.ali@gmail.com>
2019-07-15 13:16:26 -0500

    J. Randy Forbes name error

--- agencies/Treasury_Fiscal.R
+++ agencies/Treasury_Fiscal.R
@@ -54,7 +54,8 @@ data %<>%
       mutate(FROM = str_replace(FROM, "T.J. COX", "TJ COX")) %>%
       mutate(FROM = str_replace(FROM, "H. MORGAN GRIFFIN", "H. Morgan GRIFFITH")) %>%
       mutate(FROM = str_replace(FROM, "KAMALA DAVIS", "Kamala HARRIS")) %>%
-      mutate(FROM = str_replace(FROM, "MIKE BROWN", "Michael BRAUN"))
+      mutate(FROM = str_replace(FROM, "MIKE BROWN", "Michael BRAUN")) %>% 
+      mutate(FROM = str_replace(FROM, "J. FORBES", "J. Randy FORBES"))
      
     data %<>%
       mutate(FROM = ifelse(str_detect(FROM, "J FORBES") & str_detect(chamber, "House"), str_replace(FROM, "J FORBES", "James FORBES"), FROM)) %>%
