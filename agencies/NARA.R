68c3457bf68cb4549dba20e0713cca8e1ff3ba66
Maggie Nead <mnead@wisc.edu>
2019-07-16 15:47:28 -0500

    Fixed some names causing duplicates

--- agencies/NARA.R
+++ agencies/NARA.R
@@ -9,8 +9,6 @@ clean <- function(file.name) {
   data <- gs_title(file.name) %>% gs_read() # get data
   
 
-
-  
   # Make ID column. No duplicated or multi-member letters cases found
   data %<>% 
     rowid_to_column("ID")
@@ -21,6 +19,7 @@ clean <- function(file.name) {
            DATE = Date,
            FROM = `Member of Congress`)  %>%
     select(DATE, FROM, SUBJECT, everything())
+  
   
   # create agency column
   data %<>% 
@@ -34,8 +33,10 @@ clean <- function(file.name) {
   data$DATE <- gsub("-200", "-0", data$DATE)
   data$DATE %<>% multidate( c("%m-%d-%y","%m/%d/%y"))
   
+  NOdate <- data %>%
+    filter(is.na(DATE))
   
-  # #Format Typo
+  #Format Typo
   data %<>%
     mutate(FROM = str_replace(FROM, "Neugebauer", "Randy Neugebauer"))
 
@@ -46,6 +47,18 @@ clean <- function(file.name) {
   
   #test for unmatched dates
   #data %<>% filter(congress<0)
+  
+  
+  #Fixed some names causing duplicates
+  data %<>%
+    mutate(FROM = ifelse(FROM == "Rep. Steve Daines", str_replace(FROM, "Rep. Steve Daines", "Representative DAINES"), FROM)) %>%
+    mutate(FROM = ifelse(FROM == "Sen. Steve Daines", str_replace(FROM, "Sen. Steve Daines", "Senator DAINES"), FROM)) %>%
+    mutate(FROM = ifelse(FROM == "Rep. Kyrsten Sinema", str_replace(FROM, "Rep. Kyrsten Sinema", "Representative SINEMA"), FROM)) %>%
+    mutate(FROM = ifelse(FROM == "Sen. Kyrsten Sinema", str_replace(FROM, "Sen. Kyrsten Sinema", "Senator SINEMA"), FROM)) %>%
+    mutate(FROM = ifelse(FROM == "Sen. Todd Young", str_replace(FROM, "Sen. Todd Young", "Senator YOUNG"), FROM))
+    
+    
+
 
   # create first and last name variables
   data %<>% extractMemberName(members, 'FROM')
@@ -61,10 +74,19 @@ clean <- function(file.name) {
            last_name = str_c(last_name, collapse = "; "),
            first_name = str_c(first_name, collapse = "; ")) 
   
+  
+  data %<>%
+    mutate(ERROR = ifelse(str_detect(FROM, "Rep. Donna Christensen|Del. Madeleine Bordallo"), "Non Voting Member", ERROR))
+  
+  data %<>%
+    mutate(NOTES = ifelse(FROM == "Rep. Tom Rice" & is.na(last_name), "Duplicate Tom Rice", NOTES))
+  
   #Failing observations
   Unfoundnames <- data %>%
     filter(is.na(last_name),
-           is.na(ERROR))  
+           is.na(ERROR),
+           is.na(NOTES))  
+  
 
    # 
    # sample <- data %>%
