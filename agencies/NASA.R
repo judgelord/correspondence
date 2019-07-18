8a5ea735ff1d43308a25b3c290fd3cd04a8c29ba
Maggie Nead <mnead@wisc.edu>
2019-07-18 15:04:20 -0500

    Fixed chamber errors Added notes for non matched duplcate observations and observations that need FOIA

--- agencies/NASA.R
+++ agencies/NASA.R
@@ -29,7 +29,7 @@ clean <- function(file.name) {
   
   #Add comma for string split
   data %<>%
-    mutate(str_replace(FROM, "MCMORRIS RODGERS", "MCMORRIS, RODGERS"))
+    mutate(FROM = str_replace(FROM, "MCMORRIS RODGERS", "MCMORRIS, RODGERS"))
   
   # Creates duplicate rows for lines with multiple representatives
   
@@ -69,16 +69,26 @@ clean <- function(file.name) {
   
   #Format Typos
   data %<>%
-    mutate(FROM = ifelse(FROM == "JACKSON//-LEE", str_replace(FROM, "JACKSON//-LEE", "JACKSON LEE"), FROM)) %>%
+    mutate(FROM = ifelse(FROM == "JACKSON-LEE", str_replace(FROM, "JACKSON-LEE", "JACKSON LEE"), FROM)) %>%
     mutate(FROM = ifelse(FROM == "JACKSON LEE", str_replace(FROM, "JACKSON LEE", "Sheila JACKSON LEE"), FROM)) %>%
     mutate(FROM = ifelse(FROM == "VAN HOLLEN", str_replace(FROM, "VAN HOLLEN", "Christopher VAN HOLLEN"), FROM)) %>%
     mutate(FROM = ifelse(FROM == "WATSON COLEMAN", str_replace(FROM, "WATSON COLEMAN","Bonnie WATSON COLEMAN"), FROM)) %>%
     mutate(FROM = ifelse(FROM == "WASSERMAN SCHULTZ", str_replace(FROM, "WASSERMAN SCHULTZ", "Debbie WASSERMAN SCHULTZ"), FROM)) %>%
     mutate(FROM = ifelse(FROM == "MOORE CAPITO", str_replace(FROM, "MOORE CAPITO", "Shelley Moore Capito"), FROM)) %>%
+    mutate(FROM = ifelse(FROM == "CAPITO", str_replace(FROM, "CAPITO", "Shelley Moore Capito"), FROM)) %>%
     mutate(FROM = ifelse(FROM == "LUJAN. GRISHAM", str_replace(FROM, "LUJAN. GRISHAM","Michelle LUJAN"), FROM)) %>%
     mutate(FROM = ifelse(FROM == "WAXMAS", str_replace(FROM, "WAXMAS", "WAXMAN"), FROM)) %>%
     mutate(FROM = ifelse(FROM == "LUJ��N GRISHAM", str_replace(FROM,"LUJ��N GRISHAM", "Michelle LUJAN"), FROM))
   #mutate(FROM = ifelse(FROM == "Representative COBURN", ))
+  
+  data %>%
+    filter(ID == 695) %>%
+    select(FROM)
+  
+  #Chamber errors
+  data %<>%
+    mutate(chamber = ifelse(FROM == "LOFGREN", str_replace(chamber, "Senate", "House"), chamber)) %>%
+    mutate(chamber = ifelse(FROM == "KLOBUCHAR", str_replace(chamber, "House", "Senate"), chamber))
   
   #Paste chamber into FROM column
   data %<>%
@@ -93,7 +103,10 @@ clean <- function(file.name) {
   data <- extractMemberName(data, members, 'FROM')
   
   data %<>%
-    mutate(NOTES = ifelse(str_detect(FROM, "BILL NELSON") & is.na(last_name), "Bill Nelson Duplcate", NOTES))
+    mutate(NOTES = ifelse(str_detect(FROM, "BILL NELSON") & is.na(last_name), "Bill Nelson Duplcate", NOTES)) %>%
+    mutate(NOTES = ifelse(str_detect(FROM, "SHERROD BROWN") & is.na(last_name), "Sherrod Brown Duplcate", NOTES)) %>%
+    mutate(NOTES = ifelse(str_detect(FROM, "RON JOHNSON") & is.na(last_name), "Ron Johnson Duplicate", NOTES)) %>%
+    mutate(NOTES = ifelse(str_detect(FROM, "Christopher VAN HOLLEN") & is.na(last_name), "Van Hollen", NOTES))
 
 
   data$last_name <- gsub("^ |^  | $|  $", "", data$last_name)
@@ -106,16 +119,19 @@ clean <- function(file.name) {
   data %<>% select(ID, DATE, chamber,  FROM, SUBJECT, first_name, last_name, everything())
   
   
-  Unfoundnames <- data %>%
-    filter(is.na(last_name),
-           str_detect(pattern, "404error"))
-  
   #FOIA
 data %<>%
   mutate(NOTES = ifelse(FROM == "Representative ROGERS", "Multiple Rogers FOIA", NOTES)) %>%
   mutate(NOTES = ifelse(FROM == "JAMES PAUL", "FOIA", NOTES)) %>%
   mutate(NOTES = ifelse(FROM == "Representative MARTINEZ", "FOIA", NOTES)) %>%
-  mutate(NOTES = ifelse(FROM == "Representative THOMPSON", "Multiple Thompson's FOIA", NOTES))
+  mutate(NOTES = ifelse(FROM == "Representative THOMPSON", "Multiple Thompson's FOIA", NOTES)) %>%
+  mutate(NOTES = ifelse(FROM == "Representative BROOKS" & ! str_detect(congress, "112"), "Multiple Brooks FOIA", NOTES))
+  
+
+
+Unfoundnames <- data %>%
+  filter(is.na(last_name),
+         str_detect(pattern, "404error"))
 
 #Check NAs after merge  
 #unmatched <- d %>%
