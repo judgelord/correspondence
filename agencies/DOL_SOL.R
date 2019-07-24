5db2e1894ebebe6b889634cd2e8ca94abb3b6de2
fsabirinali <f.sabirin.ali@gmail.com>
2019-07-24 15:59:29 -0500

    Erroring out non member of congress

--- agencies/DOL_SOL.R
+++ agencies/DOL_SOL.R
@@ -124,7 +124,8 @@ data %<>%
   mutate(ERROR = ifelse(str_detect(FROM, "Avella, Tony"), "New York State Senate Member", ERROR)) %>%
   mutate(ERROR = ifelse(str_detect(FROM, "Zawacki, Thomas O."), "Not a member of Congress", ERROR)) %>%
   mutate(ERROR = ifelse(str_detect(FROM, "Young, Catharine M."), "New York State Senator", ERROR)) %>%
-  mutate(ERROR = ifelse(str_detect(FROM, "Wu, Portia"), "Not a member of Congress", ERROR)) 
+  mutate(ERROR = ifelse(str_detect(FROM, "Wu, Portia"), "Not a member of Congress", ERROR)) %>%
+  mutate(ERROR = ifelse(str_detect(FROM, "Winglass, Robert J."), "Not a member of Congress", ERROR))
 
 #Puts all data without a comma into last name variable and 
 #Format last name and put in last_name  
