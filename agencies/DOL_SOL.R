2da08ad29698885ab854b6eeba09af18032ab456
Your Name <you@example.com>
2019-07-24 14:27:01 -0500

    Erroring out non member of congress

--- agencies/DOL_SOL.R
+++ agencies/DOL_SOL.R
@@ -122,8 +122,8 @@ data %<>%
   mutate(ERROR = ifelse(str_detect(FROM, "Ackerman, Greg T."), "Not a member of congress", ERROR)) %>%
   mutate(ERROR = ifelse(str_detect(FROM, "Ackerman, Joyce L."), "Not a member of congress", ERROR)) %>%
   mutate(ERROR = ifelse(str_detect(FROM, "Avella, Tony"), "New York State Senate Member", ERROR)) %>%
-  mutate(ERROR = ifelse(str_detect(FROM, "Zawacki, Thomas O."), "Not a member of Congress", ERROR))
-  
+  mutate(ERROR = ifelse(str_detect(FROM, "Zawacki, Thomas O."), "Not a member of Congress", ERROR)) %>%
+  mutate(ERROR = ifelse(str_detect(FROM, "Young, Catharine M."), "New York State Senator", ERROR))
 
 
 #Puts all data without a comma into last name variable and 
