e825a70110ceb46e5403f96d8f5fe26374c11e52
fsabirinali <f.sabirin.ali@gmail.com>
2019-07-25 13:53:40 -0500

    Correcting name error in script

--- agencies/DHS_HQ.R
+++ agencies/DHS_HQ.R
@@ -138,6 +138,7 @@ data %<>% distinct() %>%
   data$FROM <- gsub("Hany", "Harry", data$FROM)
   data$FROM <- gsub("John Abney Culberson", "John Culberson", data$FROM)
   data$FROM <- gsub(".Ion Tester", "Jon Tester", data$FROM)
+  data$FROM <- gsub("(o Jo Ann S. Davis", "Jo Ann S. Davis", data$FROM)
   
   data$FROM %<>% str_replace("Charles E„ Schumer", "Charles E. Schumer")
   data$FROM %<>% str_replace("Tom A, Coburn", "Tom A. Coburn")
