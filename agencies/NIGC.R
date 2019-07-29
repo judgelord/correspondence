de0d50dc973079417124e070136d08904c6c7cc2
fsabirinali <f.sabirin.ali@gmail.com>
2019-07-29 13:14:19 -0500

    Labeling code to make more sense

--- agencies/NIGC.R
+++ agencies/NIGC.R
@@ -31,6 +31,7 @@ clean <- function(file.name) {
   data$DATE <- gsub("-200", "-0", data$DATE)
   data$DATE %<>% multidate( c("%m-%d-%y","%m/%d/%y"))
   
+  #finding unfound dates 
   NOdate <- data %>%
     filter(is.na(DATE))
   
