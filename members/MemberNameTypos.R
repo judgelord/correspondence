4ac69e0f67ee5ab3bd83d21f884703fee2421ec0
fsabirinali <f.sabirin.ali@gmail.com>
2019-07-29 16:48:34 -0500

    Getting rid of Lujan name error

--- members/MemberNameTypos.R
+++ members/MemberNameTypos.R
@@ -5,7 +5,6 @@ typos_clear <- tribble(
   ~correct, ~typos,
   "Cummings", "(Cummins|Cwnmings)",
   "Ellmers", "Ellrners",
-  "Lujan", "Luj.n",
   "TONKO", "TONKA",
   "Darrell Issa", "DarrellIssa",
   "Phil ", "Phill ", # I added a space because it seems risky to match Phill...
