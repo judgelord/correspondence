a9f68d1df0a15133e87b34c3c0bdc1ef686912d7
fsabirinali <f.sabirin.ali@gmail.com>
2019-07-30 14:28:33 -0500

    Getting rid of apostrophe s in names from subject

--- agencies/IRS.R
+++ agencies/IRS.R
@@ -26,7 +26,7 @@ data %<>% mutate(congress = as.numeric(round((year - 2001.1)/2)) + 107) # the 10
 
 #Format Typo
 data %<>%
-  mutate(SUBJECT = str_replace_all(SUBJECT, "Nancv Pelosi\\'s ", "Nancy Pelosi"))
+  mutate(SUBJECT = str_remove_all(SUBJECT, "\\'s"))
 
 #extracting members from Subject
 data <- extractMemberName(data, members, 'SUBJECT')
