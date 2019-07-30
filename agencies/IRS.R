18107c42217d49bc25683180c1cfce65d5ce22df
fsabirinali <f.sabirin.ali@gmail.com>
2019-07-30 16:42:41 -0500

    Tom Marino name error

--- agencies/IRS.R
+++ agencies/IRS.R
@@ -42,7 +42,8 @@ data %<>%
   mutate(SUBJECT = str_replace_all(SUBJECT, "John Cornvn", "John Cornyn")) %>%
   mutate(SUBJECT = str_replace_all(SUBJECT, "Vicente Gonxalez", "Vicente Gonzalez")) %>%
   mutate(SUBJECT = str_replace_all(SUBJECT, "Llovd Doaaett", "Lloyd Doggett")) %>%
-  mutate(SUBJECT = str_replace_all(SUBJECT, "Cortex Masto", "Catherine Cortez Masto"))
+  mutate(SUBJECT = str_replace_all(SUBJECT, "Cortex Masto", "Catherine Cortez Masto")) %>%
+  mutate(SUBJECT = str_replace_all(SUBJECT, "Tom Marion", "Tom Marino"))
          
 
 #extracting members from Subject
