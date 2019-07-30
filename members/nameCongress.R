42aa24aa36889cc1d1d46661b1e041a1c15d9e76
fsabirinali <f.sabirin.ali@gmail.com>
2019-07-30 13:03:23 -0500

    Adding middle name for Klein

--- members/nameCongress.R
+++ members/nameCongress.R
@@ -307,6 +307,7 @@ members <- full_join(member_search(congress = c(105:108)) %>% select(-congresses
     mutate(middle_name = ifelse(bioname == "CULBERSON, John", "Abney", middle_name)) %>% 
     mutate(middle_name = ifelse(bioname == "SCOTT, David", "Albert", middle_name)) %>% 
     mutate(middle_name = ifelse(bioname == "CRAMER, Kevin", "John", middle_name)) %>% 
+    mutate(middle_name = ifelse(bioname == "KLEIN, Ron", "Paul", middle_name)) %>%  
      
     # middle initials
     mutate(middle_initial = ifelse(bioname == "CASEY, Robert (Bob), Jr.", "P", middle_initial)) %>% 
