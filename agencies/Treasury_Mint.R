7d4cc2f05e7a828c1674bbb5f35a027c3a58bd05
fsabirinali <f.sabirin.ali@gmail.com>
2019-08-02 14:15:13 -0500

    Changing to extractmembername instead of extractmembername2

--- agencies/Treasury_Mint.R
+++ agencies/Treasury_Mint.R
@@ -45,7 +45,7 @@ clean <- function(file.name) {
  
   #Extract member names from SUBJECT
   data %<>%
-    extractMemberName2(members = members, col_name = "SUBJECT")
+    extractMemberName(members = members, col_name = "SUBJECT")
 
   
   Unfoundnames<- data %>%
