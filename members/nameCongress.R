13ac94305767a182cac2852641589cf4d8428043
fsabirinali <f.sabirin.ali@gmail.com>
2019-08-02 12:46:33 -0500

    deleting common name

--- members/nameCongress.R
+++ members/nameCongress.R
@@ -260,6 +260,7 @@ members <- full_join(member_search(congress = c(105:108)) %>% select(-congresses
     mutate(common_name = ifelse(bioname == "HUDSON, Richard", "(Rich|Rick)", common_name)) %>%
     mutate(common_name = ifelse(bioname == "HAWLEY, Joshua David", "Josh", common_name)) %>%
     mutate(common_name = ifelse(bioname == "TOWNS, Edolphus", "Ed", common_name)) %>%
+   
      
   # remove accent marks (using RegEx dot for specials, not exact matching)
     mutate(common_name = ifelse(grepl("GRIJALVA, Ra.l M.", bioname), "Raul", common_name)) %>% 
