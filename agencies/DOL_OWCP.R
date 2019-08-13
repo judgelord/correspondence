0eb1f20c01102c6ad7b702890cf6920df812f79f
fsabirinali <f.sabirin.ali@gmail.com>
2019-08-13 16:42:38 -0500

    adding distinct() to script

--- agencies/DOL_OWCP.R
+++ agencies/DOL_OWCP.R
@@ -6,7 +6,8 @@
 
 
 clean <- function(file.name) {
-  data <- gs_title(file.name) %>% gs_read() # get data
+  
+  data <- gs_title(file.name) %>% gs_read() %>% distinct() # get data
   
   
   colnames(data)[colnames(data) == 'SIMS ID'] <- 'ID'
