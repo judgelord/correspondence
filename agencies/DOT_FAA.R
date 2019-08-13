895ab51a5b903bdef0c2656976cbfca16acf3675
fsabirinali <f.sabirin.ali@gmail.com>
2019-08-13 16:33:02 -0500

    adding distinct() to script

--- agencies/DOT_FAA.R
+++ agencies/DOT_FAA.R
@@ -5,7 +5,7 @@
  
 
 clean <- function(file.name) {
-  data <- gs_title(file.name) %>% gs_read() # get data
+  data <- gs_title(file.name) %>% gs_read() %>% distinct() # get data
   
   #create ID variable
   data$ID <- c(1:nrow(data))
