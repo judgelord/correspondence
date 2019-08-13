2d84f1a36ab006fdddcf7fe35245d109c81ba933
fsabirinali <f.sabirin.ali@gmail.com>
2019-08-13 16:17:53 -0500

    adding return(data) to script

--- agencies/DOI_BSEE.R
+++ agencies/DOI_BSEE.R
@@ -7,7 +7,7 @@
 
 
 clean <- function(file.name) {
-  data <- gs_title(file.name) %>% gs_read() # get data
+  data <- gs_title(file.name) %>% gs_read() %>% distinct() # get data
   
   # create ID variable
   data$ID <- c(1:nrow(data))
@@ -82,7 +82,7 @@ clean <- function(file.name) {
   
   
   
-  
+  return(data)
   
   
 }
