46162b5ab456bb9d9b37110eac5d1e34d9e8670e
fsabirinali <f.sabirin.ali@gmail.com>
2019-08-13 16:48:42 -0500

    adding distinct() to script

--- agencies/DOL_SOL.R
+++ agencies/DOL_SOL.R
@@ -5,7 +5,8 @@
 
 
 clean <- function(file.name) {
-  data <- gs_title(file.name) %>% gs_read() # get data
+  
+  data <- gs_title(file.name) %>% gs_read() %>% distinct() # get data
   
   names(data)[names(data) == 'SIMS ID'] <- 'LetterID'
   
