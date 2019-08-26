1313ad609e79832e66c7a14c35ddb8203b2ab037
fsabirinali <fsali@wisc.edu>
2019-08-26 17:27:57 -0500

    editing name errors for ED script

--- agencies/ED.R
+++ agencies/ED.R
@@ -34,7 +34,7 @@ clean <- function(file.name) {
   
   # preprocess FROM column
   data$FROM <- gsub("( |^)The( |$)|honorable|Honorable|hon\\.|honora ble|Honorab le|Senator|Name:|Ho norable|Representataive|Honorabel", "", data$FROM, ignore.case = TRUE)
-  data$FROM <- gsub("Mr\\.|Ms\\.", "",data$FROM, ignore.case = TRUE)
+  data$FROM <- gsub("Mr\\.|Ms\\.|Mr ", "",data$FROM, ignore.case = TRUE)
   data$FROM <- gsub("Wolff", "Wolf", data$FROM)
   
   #data %<>% filter(str_detect(FROM,"Hollen, Chris Van"))
@@ -52,10 +52,15 @@ clean <- function(file.name) {
     str_replace_all("McMorris Rodgers ,", "McMORRIS RODGERS, Cathy") %>%
     str_replace_all("Ros-Lehtinen,", "ROS-LEHTINEN, Ileana") %>%
     str_replace_all("Butterfield,  G.K.", "BUTTERFIELD, George Kenneth, Jr. (G.K.)") %>%
+    str_replace_all("Butterfield,  G. K.", "BUTTERFIELD, George Kenneth, Jr. (G.K.)") %>%
     str_replace_all("Ada Alma", "ADAMS, Alma") %>%
     str_replace_all("Young,  CW Bill", "YOUNG, Charles William (Bill)") %>%
-    str_replace_all("Young,  C.W. Bill", "YOUNG, Charles William (Bill)")
-   
+    str_replace_all("Young,  C.W. Bill", "YOUNG, Charles William (Bill)") %>%
+    str_replace_all("Cornyn, Mr John", "Cornyn, John") %>%
+    str_replace_all("Barr,TheAndy", "BARR, Garland H. (Andy) IV") %>%
+    str_replace_all("Barr,TheAndy", "BARR, Garland H. (Andy) IV")
+    
+  
     
  
    
