68d113e9b8096f9de2e19ff26021f58e22d27793
fsabirinali <fsali@wisc.edu>
2019-08-27 12:51:33 -0500

    Checking for NA dates in script

--- agencies/DOD_Navy.R
+++ agencies/DOD_Navy.R
@@ -23,6 +23,11 @@ clean <- function(file.name) {
   data$DATEoriginal <- data$DATE
   data$DATE %<>% as.Date("%m/%d/%y")
   
+  #checking for dates that are NA
+  NOdate <- data %>%
+    filter(is.na(DATE))
+  
+  
   #create year and congress columns
   data %<>% mutate(year = as.numeric(substring(DATE, 1, 4)))
   data %<>% mutate(congress = as.numeric(round((year - 2001.1) / 2)) + 107) # the 107th congress began in 2001
