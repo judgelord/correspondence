86d1eb09962f3f00b10e020c6df5a9a4b5ad6800
Maggie Nead <mnead@wisc.edu>
2019-10-05 11:52:38 -0500

    Fixed wrong patrick murphy

--- agencies/DOT_FAA.R
+++ agencies/DOT_FAA.R
@@ -73,7 +73,8 @@ clean <- function(file.name) {
     mutate(FROM = ifelse(str_detect(FROM, "Johnson, Tim D-SD|Johnson, Tim R-SD|Johnson, Tim  D-SD|Johnson, Tim  R-SD"), str_replace(FROM, "Johnson, Tim", "Timothy Peter JOHNSON"), FROM)) %>%
     mutate(FROM = ifelse(str_detect(FROM, "Rogers, Mike D\\/AL"), str_replace(FROM, "Rogers, Mike", "Mike Dennis ROGERS"), FROM)) %>%
     mutate(FROM = ifelse(str_detect(FROM, "Udall, Mark E D\\/NM"), str_replace(FROM, "Udall, Mark E", "Thomas UDALL"), FROM)) %>% #check that this is true
-    mutate(FROM = ifelse(str_detect(FROM, "Rogers, Mike R-MI|Rogers, Mike J R\\/MI"), str_replace(FROM, "Rogers, Mike", "Mike J ROGERS"), FROM))
+    mutate(FROM = ifelse(str_detect(FROM, "Rogers, Mike R-MI|Rogers, Mike J R\\/MI"), str_replace(FROM, "Rogers, Mike", "Mike J ROGERS"), FROM)) %>%
+    mutate(FROM = ifelse(str_detect(FROM, "Murphy, Patrick J D-PA") & congress %in% c(114) & str_detect(SUBJECT, "Florida"), str_replace(FROM, "Murphy, Patrick J D-PA", "Murphy, Patrick E D-FL"), FROM))
   
   #Match on congress and chamber
   data %<>%
