e12ef31c91a0557db670d731f75da8f0c7a5b31a
judgelord <devin.jl@gmail.com>
2020-01-09 09:36:48 -0600

    fix duplicate matches in CRP with Susan Davis (candidate who lost), Tom Price, Tom Rice, Murphy, and Rogers #168

--- functions/read-crp.R
+++ functions/read-crp.R
@@ -7,7 +7,7 @@ source("setup.R")
 # The 6th row is names
 
 
-# 113th 
+# 115th 
 c115th <- readxl::read_xls(here("CRP Data/CRP_IDs.xls"), sheet = "Members 115th")
 
 names(c115th) <- c115th[5,]
@@ -15,7 +15,7 @@ names(c115th) <- c115th[5,]
 c115th %<>% mutate(congress = 115) %>% select("CID", "CRPName", "Party", "Office", "FECCandID","congress")
 
 
-# 113th 
+# 114th 
 c114th <- readxl::read_xls(here("CRP Data/CRP_IDs.xls"), sheet = "Members 114th")
 
 names(c114th) <- c114th[5,]
@@ -69,6 +69,10 @@ d <- rbind(c105th,
 
 crpOriginal <- d
 
+# nonmembers with the same names as members
+nonmembers <- c("Davis, Susan")
+d %<>% filter(!CRPName %in% nonmembers)
+
 d %<>% extractMemberName(members = members, col_name = "CRPName")
 
 missed <- d %>% filter(congress > 112, pattern == "404error", !is.na(CRPName), CRPName != "CRPName") %>% 
@@ -79,13 +83,62 @@ missed %>% kable()
 
 crosswalk <- d %>% select(CID, CRPName, Party, Office, FECCandID, pattern, congress) %>% 
   filter(pattern != "404error") %>% 
-  distinct() %>% full_join(members %>% select(pattern, icpsr, bioname, congress) %>% distinct()) %>% 
+  distinct() %>% left_join(members %>% select(pattern, icpsr, bioname, congress) %>% distinct()) %>% 
   select(-pattern) %>% 
   distinct()
 
-crosswalk %>% arrange(icpsr) %>% add_count(icpsr, sort = T) #%>% select(CID, CRPName, bioname)
+# corrections
+crosswalk %<>% 
+  mutate(
+    icpsr = case_when(
+      CID == "N00033091" ~ 21319, # murphy
+      # CID == "N00026160" ~ 20505, # price 
+      # CID == "N00033832" ~ 21359, # rice 
+      TRUE ~ as.double(icpsr))
+    ) %>% 
+  # drop overmatched
+  filter(!(icpsr == 20505 & CID != "N00026160"), # price
+         !(icpsr == 21359 & CID != "N00033832"), # rice
+         !(icpsr == 20301 & CID != "N00024759"),# rogers
+         !(icpsr == 20120 & CID != "N00009668")) # rogers 
 
-members %>% filter(!icpsr %in% crosswalk$icpsr) 
+# remaining problems
+crosswalk %>% arrange(icpsr) %>% 
+  add_count(icpsr, congress, sort = T) %>% 
+  select(-congress) %>%
+  distinct() %>% 
+  filter(n>1) %>%
+  kable()
+
+# missing CRP members 
+missed_CRP <- d %>% filter(!CRPName %in% crosswalk$CRPName) %>% select(CID, CRPName, Party, Office) %>% distinct()
+missed_CRP$CRPName
+
+# missing VOTEVIEW members 
+missed <- members %>% filter(!icpsr %in% crosswalk$icpsr, 
+                             chamber %in% c("House", "Senate"),
+                             congress > 112, # looks like CRP 2012 file is super incomplete, does not cover the 112th or before
+                             congress < 116) %>%
+  select(icpsr, bioname, last_name) %>% distinct()
+missed$bioname
+
+# For example, there is no Joe Lieberman, who served until the 112th
+d %>% filter(str_detect(CRPName, "Lieberman")) %>% select(congress, string, pattern, CID, CRPName)
+
+missed_CRP %>% filter(str_detect(toupper(CRPName), missed$last_name))
+
+crosswalk %>% 
+  add_count(CID, congress, sort = T) %>% 
+  select(-congress) %>%
+  distinct() %>% 
+  filter(n>1) %>%
+  kable()
+
+# Inspect for false positives
+crosswalk %>% 
+  filter(str_sub(str_to_upper(bioname), 1,4) != str_sub(str_to_upper(CRPName), 1, 4)) %>% 
+  select(-congress) %>% distinct() %>% 
+  kable()
 
 missed_CRP <- d %>% 
   filter(!CID %in% crosswalk$CID,
