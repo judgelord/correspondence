aeef10df254597324f3f29a1c1e0ab6056581f73
Julia Derzay <jderzay@wisc.edu>
2020-02-08 16:40:17 -0600

    Hey! I added some of the changes we talked about on Friday to this part of the data, since a lot of them contain similar Subjects. Thanks!

--- agencies/EOP_USTR.R
+++ agencies/EOP_USTR.R
@@ -30,4 +30,14 @@ clean <- function(file.name) {
   data <- extractMemberName(data, members, 'FROM')
   
   data %<>% select(ID, DATE,  FROM,  everything())
+  
+  data %<>%
+    mutate(TYPE = ifelse(!str_detect(TYPE, "[0-9]")& str_detect(SUBJECT, "Conference Call"),4,TYPE))%>%
+    mutate(CERTAINTY = ifelse(!str_detect(CERTAINTY, "[0-9]")& str_detect(SUBJECT, "Conference Call"),1,CERTAINTY))%>%
+    mutate(POLICY_EVENT = ifelse(!str_detect(POLICY_EVENT, "[:alnum:]")& str_detect(SUBJECT, "Conference Call"),"international agreement;trade",POLICY_EVENT))%>%
+    mutate(TYPE = ifelse(!str_detect(TYPE, "[0-9]")& str_detect(SUBJECT, "WTO"),4,TYPE))%>%
+    mutate(CERTAINTY = ifelse(!str_detect(CERTAINTY, "[0-9]")& str_detect(SUBJECT,"WTO"),1,CERTAINTY))%>%
+    mutate(POLICY_EVENT = ifelse(!str_detect(POLICY_EVENT, "[:alnum:]")& str_detect(SUBJECT, "WTO"),"international agreement",POLICY_EVENT))%>%
+    mutate(Title = ifelse(!str_detect(Title, "[:alnum:]")& str_detect(SUBJECT, "WTO"),"WTO Ruling", Title))
+          return(data)
 }
