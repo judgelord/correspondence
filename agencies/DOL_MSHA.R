edac1036dbc09b517116c70779329881422bfc5a
Maggie Nead <mnead@wisc.edu>
2020-02-29 16:37:08 -0600

    added to string split

--- agencies/DOL_MSHA.R
+++ agencies/DOL_MSHA.R
@@ -33,16 +33,12 @@ clean <- function(file.name) {
   data %<>% mutate(year = as.numeric(substring(DATE,1,4) ))
   data %<>% mutate(congress = as.numeric(round((year - 2001.1)/2)) + 107) # the 107th congress began in 2001
   
-  data %<>% 
-    mutate(FROM = str_split(FROM, ";|and")) %>% 
-    unnest(FROM)
-  
   
   data %<>% mutate(FROM = ifelse(grepl("^69",data$FROM), data$Organization, data$FROM))
   
   
   data %<>% 
-    mutate(FROM = str_split(FROM, "/|;|&")) %>% 
+    mutate(FROM = str_split(FROM, "/|;|&| and")) %>% 
     unnest(FROM)
   
   #Create variable for chamber position  (Senator or Representative)
