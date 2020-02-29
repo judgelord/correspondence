a28aac0effb701737566cc8da24c3de09d5c4d60
Maggie Nead <mnead@wisc.edu>
2020-02-29 16:22:27 -0600

    Fixed ifelse statement

--- agencies/DOL_MSHA.R
+++ agencies/DOL_MSHA.R
@@ -51,7 +51,7 @@ clean <- function(file.name) {
     mutate(FROM = ifelse(str_detect(chamber, "Senate") & !str_detect(FROM, ","), paste("Senator", FROM, sep = " "), FROM))
   
   data %<>%
-    mutate(FROM = ifelse(str_detect(FROM, "Johnson, Tim \\(Sen\\)") & congress %in% 112, str_replace(FROM, "Johnson, Tim \\(Sen\\)", "Timothy Peter JOHNSON")))
+    mutate(FROM = ifelse(str_detect(FROM, "Johnson, Tim \\(Sen\\)") & congress %in% 112, str_replace(FROM, "Johnson, Tim \\(Sen\\)", "Timothy Peter JOHNSON"), FROM))
   
   # # create separate dataset with for names with only last name
   # data2 <- data[grepl("^\\w+$", data$FROM),]
