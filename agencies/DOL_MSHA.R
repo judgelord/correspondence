0fb7edce2476ae134d312aa5abbdbbf9f2199b03
Maggie Nead <mnead@wisc.edu>
2020-02-29 23:35:47 -0600

    Added format typos and errors for non members

--- agencies/DOL_MSHA.R
+++ agencies/DOL_MSHA.R
@@ -59,7 +59,7 @@ clean <- function(file.name) {
     mutate(FROM = str_replace(FROM, "Senator  Johanns \\(Senators\\)|Senator Johanns \\(Sens\\)", "Mike JOHANNS")) %>%
     mutate(FROM = str_replace(FROM, "Aderholt Robert B \\(Cong\\,", "Aderholt, Robert")) %>%
     mutate(FROM = str_replace(FROM, "Johnson, Tim \\(Cong\\.\\)", "Timothy V JOHNSON")) %>%
-    mutate(FROM = str_replace(FROM, "Johnson, Tim \\(Sen\\.\\)", "Timothy Peter JOHNSON"))
+    mutate(FROM = str_replace(FROM, "Johnson, Tim \\(Sen\\.\\)|Johnson, Tim \\(Sen\\)", "Timothy Peter JOHNSON"))
   
   # # create separate dataset with for names with only last name
   # data2 <- data[grepl("^\\w+$", data$FROM),]
@@ -93,7 +93,7 @@ clean <- function(file.name) {
   
   data %<>%
     mutate(ERROR = ifelse(str_detect(FROM, "Horsford, Steven A\\. \\(Sen\\.\\)") & congress %in% 112, "Not yet in congress", ERROR)) %>%
-    mutate(ERROR = ifelse(str_detect(FROM, "Cousins, Steven N\\.|Cousins, Steven N|Gordon, Robert|Navarro-Cabrer, NildaM\\."), "Non member", ERROR))
+    mutate(ERROR = ifelse(str_detect(FROM, "Cousins, Steven N\\.|Cousins, Steven N|Gordon, Robert|Navarro-Cabrer, NildaM\\.|Coull1na, Steven N\\.|Cousins, Sloven N|cousins, Steven N\\."), "Non member", ERROR))
   
   
   # data%<>%
