7467458023d780a450b29ca66e6175700480f2f2
Maggie Nead <mnead@wisc.edu>
2020-03-28 22:21:22 -0500

    wrote code to draw sample, fixed typos

--- agencies/DHHS_CMS.R
+++ agencies/DHHS_CMS.R
@@ -17,19 +17,34 @@ clean <- function(file.name) {
 
   data$DATE %<>% as.Date("%m/%d/%y")
   
+  noDate <- data %>%
+    filter(is.na(DATE))
+  
+  fullFROM <- data %>%
+    filter(! is.na(From))
+  
   #create year and congress columns
   data %<>% mutate(year = as.numeric(substring(DATE,1,4) ))
   data %<>% mutate(congress = as.numeric(round((year - 2001.1)/2)) + 107) # the 107th congress began in 2001
     
   data %<>%
-    mutate(FROM = paste(`From First Name`, `From Last Name`, sep = " "))
+    mutate(FROM = ifelse(is.na(From), paste(`From First Name`, `From Last Name`, sep = " "), FROM))
+  
+  #data <- data[sample(1:nrow(data), 4000, replace=FALSE),]
+  
+  #Typos
+  data %<>%
+    mutate(FROM = str_replace(FROM, "Wm\\. Clay", "William CLAY")) %>%
+    mutate(FROM = str_replace(FROM, "Robert P\\., Jr\\. Casey", "Robert Casey")) %>%
+    mutate(FROM = str_replace(FROM, "Joe, III Manchin", "Joe Manchin"))
 
   #Extract Member names
   data %<>%
     extractMemberName(members = members, col_name = "FROM")  
   
   
-  
+  unfoundNamesSample <- sample %>%
+    filter(is.na(last_name))
   
   return(data)
   
