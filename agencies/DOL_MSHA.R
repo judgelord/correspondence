70d7eb742dd52d47de1e5bb3f1ae0d3d379fed25
Maggie Nead <mnead@wisc.edu>
2020-04-25 13:08:34 -0500

    Fixed name typos and added ERRORs for non members

--- agencies/DOL_MSHA.R
+++ agencies/DOL_MSHA.R
@@ -69,7 +69,8 @@ clean <- function(file.name) {
     mutate(FROM = str_replace(FROM, "Whltehouse, Sheldon", "WHITEHOUSE, Sheldon")) %>%
     mutate(FROM = ifelse(str_detect(FROM, "Representative  Harris \\(Congs\\)") & congress %in% 114, str_replace(FROM, "Representative  Harris \\(Congs\\)", "HARRIS, Andy"), FROM)) %>%
     mutate(FROM = ifelse(str_detect(FROM, "Senator Timothy J\\. \\(Sen\\)") & congress %in% 112, str_replace(FROM, "Senator Timothy J\\. \\(Sen\\)", "Timothy Peter JOHNSON"), FROM)) %>%
-    mutate(FROM = ifelse(str_detect(FROM, "Representative Markwayne \\(Cong\\)"), str_replace(FROM, "Representative Markwayne \\(Cong\\)", "Representative MULLIN"), FROM))
+    mutate(FROM = ifelse(str_detect(FROM, "Representative Markwayne \\(Cong\\)"), str_replace(FROM, "Representative Markwayne \\(Cong\\)", "Representative MULLIN"), FROM)) %>%
+    mutate(FROM = str_replace(FROM, "Zlnke, Ryan \\(Cong\\)", "Zinke, Ryan"))
   
   
   #Fix Chamber
@@ -104,9 +105,10 @@ clean <- function(file.name) {
   
   data %<>%
     mutate(ERROR = ifelse(str_detect(FROM, "Horsford, Steven A\\. \\(Sen\\.\\)") & congress %in% 112, "Not yet in congress", ERROR)) %>%
-    mutate(ERROR = ifelse(str_detect(FROM, "Cousins, Steven N\\.|Cousins, Steven N|Gordon, Robert|Navarro-Cabrer, NildaM\\.|Coull1na, Steven N\\.|Cousins, Sloven N|cousins, Steven N\\.|Barber, Elizabeth|Wooten, Ronald|Lewis, Elliot"), "Non member", ERROR)) %>%
-    mutate(ERROR = ifelse(str_detect(FROM, "Schermer, Barry s\\."), "judge", ERROR)) %>%
-    mutate(ERROR = ifelse(str_detect(FROM, "Pallasch, John"), "assistant secretary", ERROR))
+    mutate(ERROR = ifelse(str_detect(FROM, "Cousins, Steven N\\.|Cousins, Steven N|Gordon, Robert|Navarro-Cabrer, NildaM\\.|Coull1na, Steven N\\.|Cousins, Sloven N|cousins, Steven N\\.|Barber, Elizabeth|Wooten, Ronald|Lewis, Elliot|Beener, George R\\.|Bennazar, Zuquelra, A.J. \\(Allorneyo\\)"), "Non member", ERROR)) %>%
+    mutate(ERROR = ifelse(str_detect(FROM, "Schermer, Barry s\\.|Schermer, Barry|Schermer, Barry S \\(Judge\\)|Schermer, Barry S\\. \\(Judge\\)"), "judge", ERROR)) %>%
+    mutate(ERROR = ifelse(str_detect(FROM, "Pallasch, John"), "assistant secretary", ERROR)) %>%
+    mutate(ERROR = ifelse(str_detect(FROM, "Cuomo, Andrew M\\.\\(Gov\\.\\)"), "state legislator", ERROR))
   
   
   #Failing observations
