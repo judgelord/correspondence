8021c6baa0cc73de67bb48322cf7c9dd9bd13665
judgelord <devin.jl@gmail.com>
2020-05-22 13:10:33 -0500

    drive auth for merge

--- merge.R
+++ merge.R
@@ -5,7 +5,10 @@ source("setup.R") # clean.agency() cleans data and adds a sheet of unresolved in
 Yes # just in case R asks if we want to install dependencies 
 
 # log in to google drive
-drive_auth(email = "correspondenceresearch@gmail.com")
+drive_auth(email = "correspondenceresearch@gmail.com",
+           path = "drive-key.json")
+
+
 
 # ## make sure gmailr is set up 
 # send_message(mime(
@@ -181,7 +184,7 @@ data_list
 i <- 1
 # or choose one agency
 
-i <- which(data_list$agency == "CSOSA")
+i <- which(data_list$agency == "ABMC")
 
 d1 <- clean.agency(
   agency = as.character(data_list[i, 1]),
