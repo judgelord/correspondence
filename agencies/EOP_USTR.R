224a2dc66475b50377337e9bbdb0e089fb1f916a
judgelord <devin.jl@gmail.com>
2020-06-19 20:40:16 -0500

    dates need work #2

--- agencies/EOP_USTR.R
+++ agencies/EOP_USTR.R
@@ -1,11 +1,10 @@
 # This script defines a function clean() for google sheets of correspondence logs that may have been hand coded
 # It may also auto-code variables like TYPE based on agency-specific information
 
-# file.name <- "EOP_USTR" # for testing
-
+# file.name <- "EOP_USTR Hope" # for testing
 
 clean <- function(file.name) {
-  data <- gs_title(file.name) 
+  data <- gs_title(file.name) %>% gs_read()
   
   # LetterID = sheet row number
   data$LetterID <- 1:nrow(data)
@@ -14,16 +13,16 @@ clean <- function(file.name) {
   # join back in LetterID for distinct observations
   data <- data_distinct %>% left_join(data) %>% distinct()
   
-  data %<>% 
-    mutate(DATE = 'Date Received',
-           FROM = 'Source',
-           SUBJECT = paste(Title, 'Signature(s)'))
-  
   # create agency column
   data$agency <- file.name 
   
   # Format date, year, Congress
-  data$DATE %<>% as.Date() # FIXME
+  data$DATE %<>% multidate("%d-%b-%y", "%m/%d/%y") # FIXME
+  
+  bad.dates <- data %>% filter(is.na(DATE)) %>% .$LetterID
+  data$DATE[bad.dates]
+  
+  
   data %<>% mutate(year = as.numeric(substring(DATE,1,4) ))
   data %<>% mutate(congress = as.numeric(round((year - 2001.1)/2)) + 107) # the 107th congress began in 2001
 
