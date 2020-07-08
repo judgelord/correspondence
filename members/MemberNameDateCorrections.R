31b4bcdb83a534de699ef2335af7c81da768454b
judgelord <devin.jl@gmail.com>
2020-07-08 15:20:40 -0500

    add jefferson van drew, Justin Amash, and robert menendez #10

--- members/MemberNameDateCorrections.R
+++ members/MemberNameDateCorrections.R
@@ -1,8 +1,11 @@
-fix.member.date.coding <- function(data){
-  data %<>% 
+fix.member.date.coding <- function(d){
+  d %<>% 
     mutate(bioname = ifelse(is.na(bioname), "", bioname)) %>% 
     mutate(party_name = ifelse(is.na(party_name), "", party_name)) %>% 
     mutate(chamber = ifelse(is.na(chamber), "", chamber)) %>% 
+    # MENENDEZ, Robert appointed to senate on jan 16, 2006
+    filter(!(bioname == "MENENDEZ, Robert" & chamber == "House" & DATE > as.Date("2006-01-16"))) %>% #  not in house after 2006
+    filter(!(bioname == "MENENDEZ, Robert" & chamber == "Senate" & DATE < as.Date("2006-01-16"))) %>%  # not in senate before 2006
     filter(bioname != "PAYNE, Donald Milford" | DATE < as.Date("2012-06-03")) %>% # PAYNE Sr. died, replaced by PAYNE Jr.
     filter(bioname != "PAYNE, Donald, Jr." | DATE > as.Date("2012-06-03")) %>% # PAYNE Sr. died, replaced by PAYNE Jr.
     # mutate(icpsr = ifelse(last_name == "PAYNE" & DATE < as.Date("2012-06-03"), 15619, icpsr) ) %>% 
@@ -22,8 +25,24 @@ fix.member.date.coding <- function(data){
     filter(!(bioname == "GRIFFITH, Parker" & DATE > as.Date("2009-12-22") & party_name == "Democratic Party")) %>% # GRIFFITH, Parker changed to GOP
     filter(!(bioname == "GRIFFITH, Parker" & DATE < as.Date("2009-12-22") & party_name == "Republican Party")) %>% 
     filter(!(bioname == "GILLIBRAND, Kirsten" & DATE > as.Date("2009-01-26") & chamber == "House")) %>% # GILLIBRAND APPOINTED TO SENATE FROM HOUSE January 26, 2009
-    filter(!(bioname == "GILLIBRAND, Kirsten"& DATE < as.Date("2009-01-26") & chamber == "Senate"))
-  
+    filter(!(bioname == "GILLIBRAND, Kirsten"& DATE < as.Date("2009-01-26") & chamber == "Senate")) %>% 
+    # December 19, 2019, Van Drew announced that he would be joining the Republican Party
+    filter(!(bioname == "VAN DREW, Jefferson" & DATE < as.Date("2019-12-19") & party_name == "Republican Party")) %>% # not gop before 2019
+    filter(!(bioname == "VAN DREW, Jefferson" & DATE > as.Date("2019-12-19") & party_name == "Democratic Party")) %>% # not dem after 2019
+    # Jul 4, 2019 - Michigan Rep. Justin Amash announced Thursday he was leaving the Republican
+    filter(!(bioname == "AMASH, Justin" & DATE > as.Date("2019-07-04") & party_name == "Republican Party")) %>% # not gop after 2019
+    filter(!(bioname == "AMASH, Justin" & DATE < as.Date("2019-07-04") & party_name == "Independent")) # not ind before 2019
+    
+    
+
   # LIEBERMAN Indepedent in Committees, Democrat in voteview data. Voteview data will override, which is fine (no need to fix)
   # 
 }
+
+# congress bioname                 party_code icpsr
+# <chr>    <chr>                   <chr>      <chr>
+#   1 107      JEFFORDS, James Merrill 328;;;200  94240;;;14240
+# 2 111      SPECTER, Arlen          100;;;200  94910;;;14910
+# 3 111      GRIFFITH, Parker        200;;;100  90901;;;20901
+# 4 116      VAN DREW, Jefferson     200;;;100  91980;;;21980
+# 5 116      AMASH, Justin           328;;;200  91143;;;21143
