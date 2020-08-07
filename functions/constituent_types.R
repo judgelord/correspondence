40783ea8635d48891737551fc540b749ae357091
judgelord <devin.jl@gmail.com>
2020-08-06 20:13:25 -0500

    this function should now take in if_type and only apply coding to types in that column, but the behavior of this needs testing #196

--- functions/constituent_types.R
+++ functions/constituent_types.R
@@ -6,23 +6,24 @@ constituent_types <- gs_title(".constituent_types") %>% gs_read()
 blank_na <- . %>% replace_na(replace = " ")
 
 constituent_types %<>% 
-  select(agency, keywords, constituent_type, constituent_class, TYPE) %>% 
+  select(agency, keywords, constituent_type, constituent_class, if_type, TYPE) %>% 
   mutate_all(blank_na)
 
 # multiple keywords that indicate the same constituent type and class are separated by ";"
 constituent_types %<>% mutate(keywords = str_split(keywords, ";|,") ) %>% 
   unnest(keywords) %>% mutate(keywords = str_squish(keywords) %>% str_to_lower())
 constituent_types$keywords
+
 # a function to add constituent_type (from constituent_types sheet) to CONSTITUENT_TYPE (in the data)
 #FIXME WITH PURRR
-add_constituent_type <- function(d, keyword, constituent_type, constituent_class, type, Agency){
+add_constituent_type <- function(d, keyword, constituent_type, constituent_class, type, iftype, Agency){
   
-  d %<>% mutate(CONSTITUENT_TYPE = ifelse(str_detect(str_to_lower(SUBJECT), keyword) & agency %in% Agency , # if keword in subject
+  d %<>% mutate(CONSTITUENT_TYPE = ifelse(str_detect(str_to_lower(SUBJECT), keyword) & agency %in% Agency & (TYPE %in% iftype | iftype == " "), # if keword in subject
                                           paste(constituent_type, CONSTITUENT_TYPE, sep = ";"), # add constituent_type
                                           CONSTITUENT_TYPE) %>% 
                   str_remove_all(";NA|;NULL|^NULL|^;")) 
 
-  d %<>% mutate(CONSTITUENT_CLASS = ifelse(str_detect(str_to_lower(SUBJECT), keyword) & agency %in% Agency , # if keword in subject
+  d %<>% mutate(CONSTITUENT_CLASS = ifelse(str_detect(str_to_lower(SUBJECT), keyword) & agency %in% Agency & (TYPE %in% iftype | iftype == " "), # if keword in subject
                                           paste(constituent_class, CONSTITUENT_CLASS, sep = ";"), # add constituent_type
                                           CONSTITUENT_CLASS) %>% 
                   str_remove_all(";NA|;NULL|^NULL|^;")) 
@@ -41,6 +42,7 @@ d %<>%
                          constituent_types$constituent_type[i], 
                          constituent_types$constituent_class[i],
                          constituent_types$TYPE[i],
+                         constituent_types$if_type[i],
                          constituent_types$agency[i])
   
   d %<>% mutate_at(vars(CONSTITUENT_CLASS, CONSTITUENT_TYPE), str_squish)
