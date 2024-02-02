4f8ae1bb52e1ac71f7f2285bfbcef83f736829db
Jessie Munson <71087824+JEMunson@users.noreply.github.com>
2024-02-02 14:24:25 -0600

    Created new cleaning script for DOJ_USMS (2/2/24)

---
+++ DOJ_USMS.R
@@ -0,0 +1,62 @@
+# This script defines a function clean() for google sheets of correspondence logs that may have been hand coded
+# It may also auto-code variables like TYPE based on agency-specific information
+
+source("setup.R")
+file.name <- "DOJ_USMS" # for testing
+
+clean <- function(file.name) {
+  data_raw <- gs_title(file.name) %>% gs_read()
+  
+  # LetterID = sheet row number
+  data_raw$LetterID <- 1:nrow(data_raw)
+  
+  # select distinct observations 
+  data_distinct <- data_raw %>% select(-LetterID) %>% distinct()
+  
+  ##########################################################
+  # join back in LetterID for distinct observations
+  data <- data_distinct %>% left_join(data_raw) %>% distinct()
+  
+  # create agency column
+  data$agency <- file.name
+  
+  data$ID <- seq(1:nrow(data))
+  
+  data %<>% 
+    rename(FROM = `NAME`)
+  
+  data$DATE %<>% 
+    str_replace_all("-", "/") %>% 
+    as.Date("%m/%d/%Y")
+  
+  
+  #create year and congress columns
+  data %<>% mutate(year = as.numeric(substring(DATE,1,4) ))
+  data %<>% mutate(congress = as.numeric(round((year - 2001.1)/2)) + 107) # the 107th congress began in 2001
+  
+  #Indivdual line/member string changes
+  
+  data %<>%
+    mutate(FROM = str_to_lower(FROM))
+  
+    
+    library(legislators)
+  data %<>% 
+    legislators::extractMemberName("FROM",
+                                   congress = "congress")
+  
+  
+  return(data)
+}
+
+if(F){
+  data %>% count(is.na(icpsr)) 
+  
+  data %>% filter(is.na(icpsr)) %>% distinct(FROM, congress, DATE) %>% print(n = 194)
+  
+  data %>% filter(is.na(icpsr),
+                  str_detect(FROM, "vacant")) %>% view()
+}
+
+print(distinct(data$FROM))
+  
